ecf4c54e00db3c59fd8a0fbaa39a6467cea4bf9b
Rudra <rp2444941@gmail.com>
2023-08-22 11:24:22 +0530

    Rudra

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Items from './components/Items'
 import Products from './Products'
 import Footer from './components/Footer'
+import Section from './components/Section'
 
 function App() {
   return (
     <div className='app'>
+ <section/>
 <Products/>
 <Footer/>
     </div>
